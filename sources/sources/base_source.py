97245477c6c57c3a79fab47668739ae1db2d6bda
Avramradar <avram.aleksey@yandex.com>
2026-07-20 09:45:45 +0300

    Create base_source.py

---
+++ sources/sources/base_source.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+
+
+class BaseSource(ABC):
+    """
+    Базовый класс для всех источников данных.
+    """
+
+    @abstractmethod
+    def fetch(self):
+        """
+        Возвращает новые записи из источника.
+        """
+        raise NotImplementedError
