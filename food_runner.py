2be451c452143af420ff8eb991a438367779bdaa
Avramradar <avram.aleksey@yandex.com>
2026-07-21 19:54:33 +0300

    Update food_runner.py

--- food_runner.py
+++ food_runner.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from food_service import publish_recipe
 from sources.food_source import FoodSource
 from sources.rss_food_source import RssFoodSource
-
+from food_to_news import main as copy_food_to_news
 
 def main() -> None:
     rss = RssFoodSource()
@@ -16,7 +16,9 @@ def main() -> None:
     for post in posts:
         result = publish_recipe(post)
         print(result)
-
+        
+        if str(result).startswith("Рецепт опубликован"):
+            copy_food_to_news()
 
 if __name__ == "__main__":
     main()
